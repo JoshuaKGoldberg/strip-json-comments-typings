09916d3375d84864ea34a7c8d83a7cea5acd28e9
Josh Goldberg <josh@fullscreenmario.com>
2017-08-02 09:56:55 -0700

    Initial commit with failing typings

---
+++ test.ts
@@ -0,0 +1,26 @@
+import * as stripJsonComments from "strip-json-comments";
+
+/**
+ * Content structure for tsconfig.json files.
+ */
+export interface ITemplateStructure {
+    /**
+     * Source file paths to include.
+     */
+    files: string[];
+}
+
+/**
+ * Parses tsconfig.json files.
+ */
+export class TemplateParser {
+    /**
+     * Parses a tsconfig.json file.
+     *
+     * @param contents   Contents of a tsconfig.json file.
+     * @returns The parsed structure of the file.
+     */
+    public intake(contents: string): ITemplateStructure {
+        return JSON.parse(stripJsonComments(contents));
+    }
+}
